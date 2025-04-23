d2aaa2add4c326e6b484b5fda76a0ff1e0dd8394
pipopo <puchi.maru.milk@gmail.com>
2025-04-23 22:32:19 +0900

    add fn read_data

---
+++ src/files.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::error::Error;
+use std::io::{BufRead, BufReader};
+
+pub fn read_data(path: &str) -> Result<Vec<(f64, f64)>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let mut data = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.trim().split_whitespace().collect();
+
+        let x: f64 = parts[0].parse()?;
+        let y: f64 = parts[1].parse()?;
+        data.push((x, y));
+    }
+
+    Ok(data)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::error::Error;
+
+mod files;
+use crate::files::read_data;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let train_file = &args[1];
+    let train_data = read_data(&train_file)?;
+
+    println!("{:?}", train_data);
+    Ok(())
 }
